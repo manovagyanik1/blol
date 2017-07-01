ae6d882632f7977091bd8b2a2d0c0253dde86dfe
shubham agrawal <manovagyanik1@gmail.com>
2017-07-01 22:11:35 +0530

    making gulp and debug work at the same time

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,20 @@ var gulp = require("gulp"),
     merge = require('merge2'),
     runSequence = require("run-sequence");
 
+var shell = require('gulp-shell');
+
 var files = require('./gulp.config');
-var tsProject = ts.createProject('tsconfig.json');
+var tsProject = ts.createProject('./tsconfig.json');
 
 gulp.task('default', function(callback) {
-    runSequence('build', 'watch', callback);
+    runSequence('build', callback);
 });
 
 gulp.task('build', function(callback){
     runSequence('clean', 'copy-build', callback);
 });
 
-gulp.task('copy-build', ['copy-configs', 'scripts']);
+gulp.task('copy-build', ['copy-configs', 'typescript']);
 
 gulp.task('copy-configs', function(){
     return gulp.src('./configurations/*.json')
@@ -26,18 +28,16 @@ gulp.task('clean', function(callback){
     return del(['./build/**/*'], {force: true}, callback);
 });
 
-gulp.task('compile', function(){
-  gulp.src(files.app_files.ts)
-    .pipe(tsProject())
-    .pipe(gulp.dest('build/'))
-});
-
 gulp.task('scripts', function() {
-    return gulp.src(files.app_files.ts)
-    .pipe(tsProject())
-    .pipe(gulp.dest('build/'));
+    // return gulp.src(files.app_files.ts)
+    // .pipe(sourcemaps.init())
+    // .pipe(tsProject())
+    // .pipe(sourcemaps.write())
+    // .pipe(gulp.dest('build/'));
 });
 
-gulp.task('watch', function(){
-    gulp.watch(files.app_files.ts, ['build', 'scripts']);
-});
+gulp.task('typescript', shell.task('npm run compile'));
+
+// gulp.task('watch', function(){
+//     gulp.watch(files.app_files.ts, ['build', 'scripts']);
+// });
--- src/models/interfaces/user.ts
+++ src/models/interfaces/user.ts
@@ -2,6 +2,5 @@ export interface IUser {
   email?: string;
   fullName?: string;
   nickName?: string;
- 
   profilePicUrl?: string;
 }
