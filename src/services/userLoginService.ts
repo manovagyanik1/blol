395ef92fb48716ae4be2d1b11242f3acfa24ec79
shubham agrawal <manovagyanik1@gmail.com>
2017-07-04 16:11:53 +0530

    return jwttoken from login callback response

--- src/services/userLoginService.ts
+++ src/services/userLoginService.ts
@@ -12,37 +12,34 @@ export class UserLoginService extends BaseService {
     // return the login token if the user already exists in the db,
     // create a new user if the user is not there in the db.
     public static getLoginToken(code: String): Promise<any> {
-        return FB.api('oauth/access_token', {
-            client_id: config.get('facebook:appId'),
-            client_secret: config.get('facebook:appSecret'),
-            redirect_uri: config.get('facebook:redirectUri'),
-            code
-        }, function (res) {
-            if (!res || res.error) {
-                console.log(!res ? 'error occurred' : res.error);
-                return;
-            }
-
-        var accessToken = res.access_token;
-        var expires = res.expires ? res.expires : 0;
-        FB.options({accessToken});
-
-        FB.api('/me', function (res) {
-            if (res && res.error) {
-                if (res.error.code === 'ETIMEDOUT') {
-                    console.log('request timeout');
-                } else {
-                    console.log('error', res.error);
+        return new Promise((resolve, reject) => {
+            FB.api('oauth/access_token', {
+                client_id: config.get('facebook:appId'),
+                client_secret: config.get('facebook:appSecret'),
+                redirect_uri: config.get('facebook:redirectUri'),
+                code
+            }, function (res) {
+                if (res && res.error) {
+                    reject(res.error);
                 }
-            } else {
-                const {id, name, email} = res;
-                const tokenData = {id, name, email};
-                // get JWT token and insert into request/response
-                const jwtToken: string = JWTUtils.signJWTToken(tokenData);
-                return jwtToken;
-            }
+
+                var accessToken = res.access_token;
+                var expires = res.expires ? res.expires : 0;
+                FB.options({accessToken});
+
+                FB.api('/me', function (res) {
+                    if (res && res.error) {
+                        reject(res.error);
+                    } else {
+                        const {id, name, email} = res;
+                        const tokenData = {id, name, email};
+                        // get JWT token and insert into request/response
+                        const jwtToken: string = JWTUtils.signJWTToken(tokenData);
+                        resolve({token: jwtToken});
+                    }
+                });
+            });
         });
-    });
     }
 
     public static isNewUser(): boolean {
