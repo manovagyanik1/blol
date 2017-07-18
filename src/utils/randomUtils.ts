4144b55273e96e0d274e9c9b53d2271a283eb506
ranjith1610 <ranjith1610@gmail.com>
2017-07-18 17:18:29 +0530

    random numbers list summing up to n generator

--- src/utils/randomUtils.ts
+++ src/utils/randomUtils.ts
@@ -1,3 +1,17 @@
-export class RandomUtils {
+export default class RandomUtils {
+    public static getXRandomNumbersThatSumToN(x:number, n:number): number[] {
+        const temp = [];
+        temp.push(0);
+        temp.push(n);
+        const ret = [];
+        for ( let i = 0; i < x - 1; i++) {
+            temp.push( Math.round(Math.random() * n));
+        }
+        temp.sort();
+        for ( let i = 1; i < temp.length; i ++) {
+            ret.push(temp[i] - temp[i - 1]);
+        }
+        return ret;
+    }
 
 }
