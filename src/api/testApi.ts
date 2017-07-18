d88041b2552d7fb4b90b12f3538a23a55de115c0
shubham agrawal <manovagyanik1@gmail.com>
2017-07-18 15:11:53 +0530

    adding sentry for test

--- src/api/testApi.ts
+++ src/api/testApi.ts
@@ -14,8 +14,4 @@ export class TestApi extends BaseApi {
     public static runFacebookPostPullerCron(request: Hapi.Request, reply: Hapi.IReply) {
         Promise.all(FbPostPullerService.mainCron()).then((data) => reply(data));
     }
-
-    public static testImage(request: Hapi.Request, reply: Hapi.IReply) {
-        ImageUtils.test();
-    }
 }
