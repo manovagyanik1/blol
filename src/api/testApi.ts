fa113eaa1feaacb7b4b0b6ba61e897e8217b2647
shubham agrawal <manovagyanik1@gmail.com>
2017-07-18 20:33:31 +0530

    added admin auth check in genUtils. Needs to be tested and used

--- src/api/testApi.ts
+++ src/api/testApi.ts
@@ -1,10 +1,9 @@
 import * as Hapi from 'hapi';
 import BaseApi from "./baseApi";
 import {FbPostPullerService} from "../services/fbPostPullerService";
+import GenUtils from "../utils/genUtils";
 
 export class TestApi extends BaseApi {
-
-
     public static runFacebookPostPullerCron(request: Hapi.Request, reply: Hapi.IReply) {
         Promise.all(FbPostPullerService.fetchAllPostsFromFbPage()).then((data) => reply(data));
     }
