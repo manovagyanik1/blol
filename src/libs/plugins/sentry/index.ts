24e8ada6112271300d5bf6c87d5e3c159a620fd2
shubham agrawal <manovagyanik1@gmail.com>
2017-07-18 17:11:21 +0530

    adding sentry

---
+++ src/libs/plugins/sentry/index.ts
@@ -0,0 +1,32 @@
+import { IPlugin } from "../interfaces";
+import * as Hapi from "hapi";
+import container from "../../ioc";
+import { IServerConfig } from "../../../../configurations/interfaces";
+const config = container.get<IServerConfig>("IServerConfig");
+
+export default (): IPlugin => {
+    return {
+        register: (server: Hapi.Server) => {
+            server.register({
+                register: require('hapi-raven-boom'),
+                options: {
+                    dsn: config.get('sentry:DSN'),
+                    settings: {
+                        captureUnhandledRejections: true
+                    },
+                    tags: ['sentry-errors']
+                }
+            }, (error) => {
+                if (error) {
+                    console.log('error', error);
+                }
+            });
+        },
+        info: () => {
+            return {
+                name: "Hapi-auth-jwt2 Auth",
+                version: "1.0.0"
+            };
+        }
+    };
+};
